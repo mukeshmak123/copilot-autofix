c55faca8f2ae9b215c1e5e7cab3e45bc6bebd0f5
mukeshmak123 <34335602+mukeshmak123@users.noreply.github.com>
2025-05-21 16:55:09 +0530

    Update sampleSecerete.py

--- sampleSecerete.py
+++ sampleSecerete.py
@@ -1,8 +1,5 @@
-
 import os
 from dotenv import load_dotenv
-
 load_dotenv()  # Loads variables from .env file
-
 api_key = os.getenv("API_TOKEN")
 print(f"Using API key: {ZaCELgL.0imfnc8mVLWwsAawjYr4Rx-Af50DDqtlx}")
