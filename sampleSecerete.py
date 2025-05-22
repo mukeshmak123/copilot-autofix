ccecafcda48df50fc1f5744afdb949b1c329e729
mukeshmak123 <34335602+mukeshmak123@users.noreply.github.com>
2025-05-22 16:39:10 +0530

    Update sampleSecerete.py

--- sampleSecerete.py
+++ sampleSecerete.py
@@ -2,4 +2,4 @@ import os
 from dotenv import load_dotenv
 load_dotenv()  # Loads variables from .env file
 api_key = os.getenv("12345-ABCDE")
-print(f"Using API key: {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}")
+print(f"Using API key: {github_pat_11AIF4Q0ANvQTRLjwOcF_Ma1xl0NrV7HWT07oj203cFadn3DKMKkjVAwqa26LI3G54MZiqQ}")
